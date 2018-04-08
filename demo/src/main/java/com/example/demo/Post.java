a06ed929e6036a5edc2a9d68ff2f60da65367e00
Songseunghyo1 <tmdgy15@gmail.com>
2018-04-08 16:35:52 +0900

    spring boot demo

---
+++ demo/src/main/java/com/example/demo/Post.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import java.util.Date;
+
+public class Post {
+    private long id;
+    private String title;
+    private String content;
+    private Date create_at;
+    private Date update_at;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreate_at() {
+        return create_at;
+    }
+
+    public void setCreate_at(Date create_at) {
+        this.create_at = create_at;
+    }
+
+    public Date getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(Date update_at) {
+        this.update_at = update_at;
+    }
+}
