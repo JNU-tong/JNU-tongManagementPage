d533b22ae68f1d2e2f46f596ed586d176bd1c536
Songseunghyo1 <tmdgy15@gmail.com>
2018-04-09 11:03:48 +0900

    new Login class(spring boot)

---
+++ demo/src/main/java/com/example/demo/Role.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private int id;
+    @Column(name = "role")
+    private String role;
+}
