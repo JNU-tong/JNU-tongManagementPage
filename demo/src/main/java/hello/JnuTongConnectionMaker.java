4d4406fa9ede088e1dd123fce302a40c21e8b358
Songseunghyo1 <tmdgy15@gmail.com>
2018-05-05 17:05:42 +0900

    use lombok

--- demo/src/main/java/hello/JnuTongConnectionMaker.java
+++ demo/src/main/java/hello/JnuTongConnectionMaker.java
@@ -1,15 +1,25 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JnuTongConnectionMaker implements ConnectionMaker {
+    @Value("${db.classname}")
+    String classname= "com.mysql.jdbc.Driver";
+    @Value("${db.url}")
+    String url = "jdbc:mysql://localhost/jnutongmanagers";
+    @Value("${db. username}")
+    String username = "root";
+    @Value("${db.password}")
+    String password = "sslabflask";
 
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName(classname);
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost/jnutongmanagers", "root", "sslabflask");
+                url, username, password);
     }
 }
--- demo/src/main/java/hello/Manager.java
+++ demo/src/main/java/hello/Manager.java
@@ -1,42 +1,13 @@
 package hello;
 
+import lombok.Data;
+
+@Data
 public class Manager {
     private Long num;
     private String id;
     private String role;
     private String password;
-
-    public Long getNum() {
-        return num;
-    }
-
-    public void setNum(Long num) {
-        this.num = num;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     @Override
     public String toString() {
